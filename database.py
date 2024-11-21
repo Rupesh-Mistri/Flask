06e6b4477f52a884e31f8b48cc0c330dab239e7e
Rupesh-Mistri <rupesh.kumar805@gmail.com>
2024-11-21 08:09:14 +0530

    db changed

--- database.py
+++ database.py
@@ -5,8 +5,10 @@ from models import Base
 
 DATABASE_URL = "sqlite:///database.db"
 
-engine = create_engine(DATABASE_URL)
-Base.metadata.create_all(engine)
+# engine = create_engine(DATABASE_URL)
+# Base.metadata.create_all(engine)
+
+engine=create_engine("mysql+pymysql:://rupesh:fyndtest123@rupesh.mysql.pythonanywhere-services.com/rupesh$fynd")
 
 Session = sessionmaker(bind = engine)
 session = Session()
